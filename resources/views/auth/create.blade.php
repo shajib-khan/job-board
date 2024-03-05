93074995eb23a1e16aa1498fe4ecf22a7c82c754
shajib-khan <shajibkhan946@gmail.com>
2024-03-05 19:40:18 +0600

    Refactor Time: Flash Errors

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -8,21 +8,20 @@
         @csrf
 
         <div class="mb-8">
-          <label for="email"
-            class="mb-2 block text-sm font-medium text-slate-900">E-mail</label>
+          <x-label for="email" :required="true">E-mail</x-label>
           <x-text-input name="email" />
         </div>
 
         <div class="mb-8">
-          <label for="password" class="mb-2 block text-sm font-medium text-slate-900">
+          <x-label for="password" :required="true">
             Password
-          </label>
+          </x-label>
           <x-text-input name="password" type="password" />
         </div>
 
         <div class="mb-8 flex justify-between text-sm font-medium">
           <div>
-            <div class="flex items-center space-x-2 px-10">
+            <div class="flex items-center space-x-2">
               <input type="checkbox" name="remember"
                 class="rounded-sm border border-slate-400">
               <label for="remember">Remember me</label>
